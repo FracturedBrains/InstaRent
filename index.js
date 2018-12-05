49813a288f35095e53adb28d53b2b90ea118fa91
SCSFMAC4 <scsfmac4@Admins-MacBook-Air.local>
2018-12-04 18:20:07 -0800

    setup ititial routing

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const app = express()
+const ejs = require("ejs");
+
+app.set('view engine', 'ejs');
 app.get('/', (req,res) => {
   res.render('home')
 })
-app.post('/login', (req,res) => {
-})
-app.listen(3000,'LIVE ON 3000')
+// app.post('/login', (req,res) => {
+// })
+app.listen(3000, () => console.log('LIVE ON 3000'));
